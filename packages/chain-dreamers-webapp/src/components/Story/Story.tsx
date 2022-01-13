f31c43d8f1fb86b02f94da4636d1c58507af518a
Georges Biaux <georges.biaux@gadz.org>
2022-01-13 12:23:43 +0100

    Add sneak peak to story page

--- packages/chain-dreamers-webapp/src/components/Story/Story.tsx
+++ packages/chain-dreamers-webapp/src/components/Story/Story.tsx
@@ -5,6 +5,7 @@ import background from "../Home/background.png";
 import Content from "../Content/Content";
 import Link from "@mui/material/Link";
 import spiceEffect from "./spice-effect.png";
+import sneakPeak from "./sneak-peak.png";
 
 function Story() {
   return (
@@ -87,6 +88,28 @@ function Story() {
                 src={spiceEffect}
                 className="spice-effects"
               />
+            </Box>
+          </Content>
+          <Content title="Sneak peak">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <Box
+              sx={{
+                textAlign: "center",
+                "& .sneak-peak": {
+                  maxWidth: "848px",
+                  width: "100%",
+                },
+              }}
+            >
+              <img alt="Sneak peak" src={sneakPeak} className="sneak-peak" />
             </Box>
           </Content>
         </Box>
