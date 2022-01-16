d95978344eb019f352358d99b7af67c4ca386f5c
Georges Biaux <georges.biaux@gadz.org>
2022-01-16 17:58:41 +0100

    Add image

--- packages/chain-dreamers-webapp/src/components/Team/Team.tsx
+++ packages/chain-dreamers-webapp/src/components/Team/Team.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Box from "@mui/material/Box";
 import PageTitle from "../PageTitle/PageTitle";
 import TeamMember from "../TeamMember/TeamMember";
-import photo from "./photo.png";
 import { useMediaQuery } from "@mui/material";
 import banner from "./banner.png";
+import clement from "./clement.png";
 import jaz from "./jaz.png";
 import georges from "./georges.png";
 import law from "./law.png";
 import come from "./come.png";
+import Link from "@mui/material/Link";
 
 function Team() {
   const isMobile = useMediaQuery("(max-width:915px)");
@@ -29,14 +30,38 @@ function Team() {
             Proud holder of a Candy Shop on-chain, passionate web3 product guy
             off-chain.
           </TeamMember>
-          <TeamMember name="Clément" image={photo}>
+          <TeamMember name="Clément" image={clement}>
             scientist and engineer, currently experimenting in solidity.
           </TeamMember>
           <TeamMember name="Law" image={law}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
-            duis iaculis ultrices lectus elit, pharetra donec venenatis. Nulla
-            risus lobortis congue a, tellus pharetra tempus tincidunt arcu.
-            Vitae orci porta nulla scelerisque{" "}
+            <Box>Graphic Designer - Illustrateur</Box>
+            <Box marginTop="6px">
+              <Link
+                href="https://www.instagram.com/antoinelalou/"
+                target="_blank"
+                sx={{ color: "#44DFFD" }}
+              >
+                @antoinelalou
+              </Link>
+            </Box>
+            <Box marginTop="6px">
+              <Link
+                href="https://www.instagram.com/micmac.industrie/"
+                target="_blank"
+                sx={{ color: "#44DFFD" }}
+              >
+                @micmac.industrie
+              </Link>
+            </Box>
+            <Box marginTop="6px">
+              <Link
+                href="https://opensea.io/collection/focus-inside"
+                target="_blank"
+                sx={{ color: "#44DFFD" }}
+              >
+                opensea
+              </Link>
+            </Box>
           </TeamMember>
           <TeamMember name="Côme" image={come}>
             Product designer based in Paris, building the future of NFT mint
