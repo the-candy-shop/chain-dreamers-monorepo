af1af681bde1af20be862696f006c1d82b2d6566
Georges Biaux <georges.biaux@gadz.org>
2022-01-20 13:38:43 +0100

    New api version

--- packages/chain-dreamers-api/eth-sdk/eth-sdk.config.ts
+++ packages/chain-dreamers-api/eth-sdk/eth-sdk.config.ts
@@ -6,10 +6,6 @@ const config: EthSdkConfig = {
       runnersBaseRenderer: "0xfDac77881ff861fF76a83cc43a1be3C317c6A1cC",
       runners: "0x97597002980134beA46250Aa0510C9B90d87A587",
     },
-    rinkeby: {
-      runners: "0x8756eD53f41883A6cEE38C15D775ec2604722869",
-      dreamers: "0xd767Fb9f8a338C91fB9B5e29ecD8Cf9af7C2B630",
-    },
   },
   etherscanKey: process.env.ETHERSCAN_API_KEY,
 };
--- packages/chain-dreamers-api/handler.ts
+++ packages/chain-dreamers-api/handler.ts
@@ -1,35 +1,50 @@
-import { BigNumber, ethers, utils } from "ethers";
-import { getMainnetSdk } from "@dethcrypto/eth-sdk-client";
+import { ethers } from "ethers";
 import * as valise from "./valise.json";
 
+const DreamersAbi = [
+  "function getTokenData(uint256 _dna) public view returns (uint16[13] memory traitIndexes)",
+];
+const RunnersAbi = [
+  "function getDna(uint256 _tokenId) external view returns (uint256)",
+];
+
+const Provider = ethers.getDefaultProvider(process.env.NETWORK, {
+  infura: process.env.INFURA_ID,
+  etherscan: process.env.ETHERSCAN_API_KEY,
+  alchemy: process.env.ALCHEMY_API_KEY,
+  pocket: process.env.POCKET_APPLICATION_KEY,
+});
+const Signer = ethers.Wallet.createRandom().connect(Provider);
+
+const RunnersContract = new ethers.Contract(
+  process.env.RUNNER_CONTRACT_ADDRESS || "",
+  RunnersAbi,
+  Signer
+);
+
+const DreamersRendererContract = new ethers.Contract(
+  process.env.DREAMERS_RENDERER_ADDRESS || "",
+  DreamersAbi,
+  Signer
+);
+
 export const hello = async (event) => {
-  const mainnetProvider = ethers.getDefaultProvider("mainnet", {
-    infura: process.env.INFURA_ID,
-    etherscan: process.env.ETHERSCAN_API_KEY,
-    alchemy: process.env.ALCHEMY_API_KEY,
-    pocket: process.env.POCKET_APPLICATION_KEY,
-  });
-  const defaultSigner = ethers.Wallet.createRandom().connect(mainnetProvider);
-  const sdk = getMainnetSdk(defaultSigner);
   const id = event.pathParameters.id;
 
-  const dna = await sdk.runners.getDna(id);
+  const dna = await RunnersContract.getDna(id);
   console.log("dna", dna);
 
-  const [layerStructure] = await sdk.runnersBaseRenderer.getTokenData(dna);
-
-  for (const layer of layerStructure) {
-    console.log("hexString", layer.hexString);
-  }
+  const tokenData = await DreamersRendererContract.getTokenData(dna);
+  console.log("tokenData", tokenData);
 
   let svg =
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283.5 283.5">';
 
-  /* for (const id of ids) {
+  for (const id of tokenData) {
     if (valise[id]) {
       svg += valise[id];
     }
-  } */
+  }
 
   svg += "</svg>";
 
