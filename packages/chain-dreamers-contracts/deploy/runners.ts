d706d2bc8990bb8a219081d245b4256dd47346d7
Clement Walter <clement0walter@gmail.com>
2022-01-26 12:20:15 +0100

    Clean runners' stuffes

--- packages/chain-dreamers-contracts/deploy/runners.ts
+++ packages/chain-dreamers-contracts/deploy/runners.ts
@@ -8,30 +8,28 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   if (hre.network.tags.mainnet) {
     return;
   }
-  const { deployments, getNamedAccounts, network } = hre;
+  const { deployments, getNamedAccounts } = hre;
   const { deploy, execute } = deployments;
 
   const { deployer } = await getNamedAccounts();
 
-  const tx = await deploy("ChainRunnersBaseRenderer", {
+  const tx = await deploy("ChainRunnersLayerRenderer", {
     from: deployer,
     log: true,
   });
-  if (!network.tags.mainnet) {
-    await deploy("ChainRunners", {
+  await deploy("ChainRunners", {
+    from: deployer,
+    log: true,
+  });
+  await execute(
+    "ChainRunners",
+    {
       from: deployer,
       log: true,
-    });
-    await execute(
-      "ChainRunners",
-      {
-        from: deployer,
-        log: true,
-      },
-      "setRenderingContractAddress",
-      tx.address
-    );
-  }
+    },
+    "setRenderingContractAddress",
+    tx.address
+  );
 };
 
 export default func;
