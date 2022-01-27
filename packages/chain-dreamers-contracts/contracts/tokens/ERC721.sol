e89f7bcddd6ec7370f6debcb01d6ca29f3dc604d
Clement Walter <clement0walter@gmail.com>
2022-01-27 10:22:40 +0100

    Fix ChainDreamers balanceOf

--- packages/chain-dreamers-contracts/contracts/tokens/ERC721.sol
+++ packages/chain-dreamers-contracts/contracts/tokens/ERC721.sol
@@ -351,7 +351,7 @@ abstract contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
             candies >>= 1;
             emit Transfer(address(0), to, tokenId);
         }
-        _balances[to] += uint16(tokenIds.length);
+        _balances[to] += uint16(tokenIds.length) / 2;
 
         require(
             _checkOnERC721Received(
