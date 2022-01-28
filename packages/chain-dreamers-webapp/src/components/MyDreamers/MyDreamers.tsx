55eeea3880222bb8baa2a47caec73879b9d7fc64
Georges Biaux <georges.biaux@gadz.org>
2022-01-28 15:21:36 +0100

    Add button go to candy shop to my dreamers

--- packages/chain-dreamers-webapp/src/components/MyDreamers/MyDreamers.tsx
+++ packages/chain-dreamers-webapp/src/components/MyDreamers/MyDreamers.tsx
@@ -3,11 +3,19 @@ import Box from "@mui/material/Box";
 import { useDreamersContract } from "../../hooks/useDreamersContract";
 import ShopPanels from "../ShopPanels/ShopPanels";
 import config from "../../config";
+import Typist from "react-typist";
+import Button from "@mui/lab/LoadingButton";
+import { useNavigate } from "react-router-dom";
 
 const apiBaseUrl = config.app.apiBaseUrl;
 
 function MyDreamers() {
+  const navigate = useNavigate();
   const { dreamersIds } = useDreamersContract();
+
+  const goToTheCandyShop = React.useCallback(() => {
+    navigate("/candy-shop");
+  }, [navigate]);
 
   return (
     <Box display="flex" flexDirection="column" minHeight="calc(100vh - 191px)">
@@ -33,6 +41,47 @@ function MyDreamers() {
                   </Box>
                 </Box>
               ))}
+              {dreamersIds.length === 0 && (
+                <Box display="flex" flexDirection="column" alignItems="center">
+                  <Box
+                    sx={{
+                      fontSize: "20px",
+                      fontFamily: "Share Tech Mono",
+                      padding: "32px 32px 64px 32px",
+                      textAlign: "center",
+                      color: "white",
+                    }}
+                  >
+                    <Typist avgTypingDelay={20}>
+                      You have no dreamers yet...
+                    </Typist>
+                  </Box>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      marginTop: "20px",
+                      fontSize: "20px",
+                      fontWeight: 600,
+                      padding: "12px 24px",
+                      color: "black",
+                      background: "#44DFFD",
+                      width: "340px",
+                      textTransform: "uppercase",
+
+                      "&.Mui-disabled": {
+                        background: "rgba(68,223,253,.2)",
+                      },
+
+                      "&:hover": {
+                        background: "#44DFFD",
+                      },
+                    }}
+                    onClick={goToTheCandyShop}
+                  >
+                    Go to the Candy Shop
+                  </Button>
+                </Box>
+              )}
             </Box>
           </ShopPanels>
         </Box>
