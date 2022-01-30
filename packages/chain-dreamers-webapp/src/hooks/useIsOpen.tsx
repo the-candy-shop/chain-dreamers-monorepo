2145e01e0e619640dc1323cb9b26765a49357ada
Georges Biaux <georges.biaux@gadz.org>
2022-01-30 16:21:07 +0100

    Rename rehabcenter => jail

--- packages/chain-dreamers-webapp/src/hooks/useIsOpen.tsx
+++ packages/chain-dreamers-webapp/src/hooks/useIsOpen.tsx
@@ -5,43 +5,44 @@ import { closeDate } from "../config";
 
 export const useIsOpen = (): {
   isCandyShopOpen: boolean;
-  isRehabCenterShopOpen: boolean;
+  isJailOpen: boolean;
 } => {
   const now = React.useRef<Dayjs>(dayjs());
   const { isLaunched } = useIsLaunched();
   const [isCandyShopOpen, setIsCandyShopOpen] = React.useState<boolean>(
     isLaunched && closeDate.isAfter(now.current)
   );
-  const [isRehabCenterShopOpen, setIsRehabCenterShopOpen] =
-    React.useState<boolean>(closeDate.isBefore(now.current));
+  const [isJailOpen, setIsJailOpen] = React.useState<boolean>(
+    closeDate.isBefore(now.current)
+  );
 
   React.useEffect(() => {
     let interval: NodeJS.Timeout | undefined = undefined;
 
-    if (!isRehabCenterShopOpen) {
+    if (!isJailOpen) {
       interval = setInterval(() => {
         now.current = dayjs();
         const newIsCandyShopOpen = isLaunched && closeDate.isAfter(now.current);
-        const newIsRehabCenterShopOpen = closeDate.isBefore(now.current);
+        const newIsJailOpen = closeDate.isBefore(now.current);
 
         if (newIsCandyShopOpen !== isCandyShopOpen) {
           setIsCandyShopOpen(newIsCandyShopOpen);
         }
-        if (newIsRehabCenterShopOpen !== isRehabCenterShopOpen) {
-          setIsRehabCenterShopOpen(newIsRehabCenterShopOpen);
+        if (newIsJailOpen !== isJailOpen) {
+          setIsJailOpen(newIsJailOpen);
         }
       }, 1000);
-    } else if (isRehabCenterShopOpen && interval) {
+    } else if (isJailOpen && interval) {
       clearInterval(interval);
     }
 
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [isRehabCenterShopOpen, isCandyShopOpen, isLaunched]);
+  }, [isJailOpen, isCandyShopOpen, isLaunched]);
 
   return {
     isCandyShopOpen,
-    isRehabCenterShopOpen,
+    isJailOpen,
   };
 };
