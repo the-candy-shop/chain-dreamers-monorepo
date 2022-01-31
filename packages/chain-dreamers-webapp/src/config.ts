c35dc686eb681f8648ad3c0756836335d4f78036
Georges Biaux <georges.biaux@gadz.org>
2022-01-31 21:14:38 +0100

    Update deploy config

--- packages/chain-dreamers-webapp/src/config.ts
+++ packages/chain-dreamers-webapp/src/config.ts
@@ -21,8 +21,7 @@ export const CHAIN_ID: SupportedChains = parseInt(
   process.env.REACT_APP_CHAIN_ID ?? "1"
 );
 
-export const INFURA_PROJECT_ID =
-  process.env.REACT_APP_INFURA_PROJECT_ID ?? "ee2097faedca465fbbf6fe2344d76808";
+export const INFURA_PROJECT_ID = process.env.REACT_APP_INFURA_PROJECT_ID ?? "";
 
 export const createNetworkHttpUrl = (network: string): string => {
   const custom = process.env[`REACT_APP_${network.toUpperCase()}_JSONRPC`];
