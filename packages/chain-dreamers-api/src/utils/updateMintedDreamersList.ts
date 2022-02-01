27c04fc07ec86faccfdca6d39fa349870a6a371e
Georges Biaux <georges.biaux@gadz.org>
2022-02-01 08:52:11 +0100

    Optimize api endpoints

--- packages/chain-dreamers-api/src/utils/updateMintedDreamersList.ts
+++ packages/chain-dreamers-api/src/utils/updateMintedDreamersList.ts
@@ -5,12 +5,26 @@ const TABLE_NAME = process.env.API_CHAIN_DREAMERS_TABLE_NAME || "";
 const DynamoDBClient = new AWS.DynamoDB.DocumentClient();
 
 export const updateMintedDreamersList = async (id: number): Promise<void> => {
-  const mintedDreamers = await getCurrentMintedDreamersList();
+  try {
+    const params = {
+      TableName: TABLE_NAME,
+      Key: {
+        key: documentKey,
+      },
+      UpdateExpression: "set #ids_value = list_append (#ids_value, :idList)",
+      ConditionExpression: "not contains (#ids_value, :id)",
+      ExpressionAttributeValues: {
+        ":id": id,
+        ":idList": [id],
+      },
+      ExpressionAttributeNames: {
+        "#ids_value": "value",
+      },
+      ReturnValues: "UPDATED_NEW",
+    };
 
-  if (!mintedDreamers.includes(id)) {
-    mintedDreamers.push(id);
-    await setCurrentMintedDreamersList(mintedDreamers);
-  }
+    await DynamoDBClient.update(params).promise();
+  } catch {}
 };
 
 export async function getCurrentMintedDreamersList(): Promise<number[]> {
@@ -28,16 +42,4 @@ export async function getCurrentMintedDreamersList(): Promise<number[]> {
   }
 
   return [];
-}
-
-async function setCurrentMintedDreamersList(value: number[]): Promise<void> {
-  const params = {
-    TableName: TABLE_NAME,
-    Item: {
-      key: documentKey,
-      value: value,
-    },
-  };
-
-  await DynamoDBClient.put(params).promise();
 }
