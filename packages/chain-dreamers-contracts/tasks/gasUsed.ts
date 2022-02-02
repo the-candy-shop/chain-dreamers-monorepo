55fa5c12dde6964456a6c8772633100d28a67c14
Clement Walter <clement0walter@gmail.com>
2022-02-02 16:53:12 +0100

    Use .cumulativeGasUsed in gas-used task

--- packages/chain-dreamers-contracts/tasks/gasUsed.ts
+++ packages/chain-dreamers-contracts/tasks/gasUsed.ts
@@ -5,7 +5,7 @@ task("gas-used", "Compute the total gas used for deployment").setAction(
     const contracts = await deployments.all();
     let gas = 0;
     for (const contract of Object.values(contracts)) {
-      gas += parseInt(contract.receipt?.gasUsed as string);
+      gas += parseInt(contract.receipt?.cumulativeGasUsed as string);
     }
     console.log(`Total gas used: ${gas}`);
   }
