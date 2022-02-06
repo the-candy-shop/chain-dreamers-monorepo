ce776d573a76c18afe10155d27417aa22d9c638a
Georges Biaux <georges.biaux@gadz.org>
2022-02-06 09:00:52 +0100

    Update og endpoint

--- packages/chain-dreamers-api/src/endpoints/og/metadata.ts
+++ packages/chain-dreamers-api/src/endpoints/og/metadata.ts
@@ -1,20 +1,40 @@
 import { getS3Json } from "../../utils/getS3Json";
+import { storeS3Json } from "../../utils/storeS3Json";
 
-export const metadata = async () => {
-  const s3Json = await getS3Json("og/metadata", "1");
+export const metadata = async (event) => {
+  const id = event.pathParameters.id;
+  const noCache =
+    event.queryStringParameters && event.queryStringParameters["no-cache"];
 
-  if (!s3Json) {
+  const s3Json = await getS3Json("metadata", id);
+
+  if (s3Json && !noCache) {
     return {
-      statusCode: 404,
-      body: JSON.stringify({
-        error: `Og metadata could not be found`,
-      }),
+      statusCode: 200,
+      body: s3Json,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   }
 
+  const metadata = {
+    name: "Chain Dreamers OG Badge",
+    description:
+      "A sign you were one of the very firsts to support the Chain Dreamers",
+    image:
+      process.env.API_DOMAIN_BASE_PATH === ""
+        ? `https://${process.env.API_DOMAIN_NAME}/og/tokens/${id}/img`
+        : `https://${process.env.API_DOMAIN_NAME}/${process.env.API_DOMAIN_BASE_PATH}/og/tokens/${id}/img`,
+  };
+
+  storeS3Json("metadata", id, metadata);
+
   return {
     statusCode: 200,
-    body: s3Json,
+    body: JSON.stringify(metadata),
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
