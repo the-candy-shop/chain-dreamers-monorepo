95a0dae3abb158553ac405020807288c0788bca5
Georges Biaux <georges.biaux@gadz.org>
2022-02-06 11:12:23 +0100

    Fix deploy

--- packages/chain-dreamers-api/eth-sdk/eth-sdk.config.ts
+++ packages/chain-dreamers-api/eth-sdk/eth-sdk.config.ts
@@ -21,9 +21,7 @@ const config: EthSdkConfig = {
   contracts: {
     main: contracts as Record<string, `0x${string}`>,
     originalRunner: {
-      runners:
-        (contracts.ChainRunners as `0x${string}`) ||
-        (process.env.RUNNERS_ORIGINAL_CONTRACT_ADDRESS as `0x${string}`),
+      runners: process.env.RUNNERS_ORIGINAL_CONTRACT_ADDRESS as `0x${string}`,
     },
   },
   etherscanURLs: {
