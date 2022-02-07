c1ed375ac5bb9f56e0579012d588ccda95b16d1b
Georges Biaux <georges.biaux@gadz.org>
2022-02-07 23:28:10 +0100

    Fix link

--- packages/chain-dreamers-webapp/src/components/OnChainStorage/OnChainStorage.tsx
+++ packages/chain-dreamers-webapp/src/components/OnChainStorage/OnChainStorage.tsx
@@ -425,7 +425,7 @@ function OnChainStorage() {
               Chain Dreamers is another attempt at bringing more creativity to
               the chain, we hope that you like it! We are only getting starting
               with the{" "}
-              <Link href="https://thecandieshop.wtf" target="_blank">
+              <Link href="https://thecandyshop.wtf" target="_blank">
                 Candy Shop
               </Link>
               , our innovative lab. With each new candy we will strive to push
